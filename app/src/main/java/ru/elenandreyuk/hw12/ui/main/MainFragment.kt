8b7cdd4ab7f86195c256c2871c0a8b0cd887279c
Elena <femtochka@yahoo.com>
2024-03-20 15:43:31 +0300

    hw13

--- app/src/main/java/ru/elenandreyuk/hw12/ui/main/MainFragment.kt
+++ app/src/main/java/ru/elenandreyuk/hw12/ui/main/MainFragment.kt
@@ -1,60 +1,57 @@
 package ru.elenandreyuk.hw12.ui.main
 
 import android.os.Bundle
-import android.text.Editable
-import android.text.TextWatcher
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.os.bundleOf
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.launch
 import ru.elenandreyuk.hw12.databinding.FragmentMainBinding
 
+
 class MainFragment : Fragment() {
-
-    companion object {
-        fun newInstance() = MainFragment()
-    }
-
-        private val viewModel: MainViewModel by viewModels()
+    private lateinit var  binding: FragmentMainBinding
+    private val viewModel: MainViewModel by viewModels()
+    private  lateinit var searchResult: LiveData<String>
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val binding = FragmentMainBinding.inflate(layoutInflater, container, false)
-        if(binding.editText.text?.length ?:0 < 3){
-            binding.buttonSearch.isEnabled = false
+        savedInstanceState?.let{
+            bundle ->  bundle.getString("KEY")
         }
-
-        viewModel.isSearching.observe(viewLifecycleOwner, { isSearching ->
-            binding.progress.visibility = if (isSearching) View.VISIBLE else View.GONE
-        })
-
-        viewModel.searchResult.observe(viewLifecycleOwner, { result ->
-            binding.textView.text = result
-        })
-
-        binding.editText.addTextChangedListener(object : TextWatcher {
-            override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {
-            }
-
-            override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
-            }
-
-            // После изменения текста
-            override fun afterTextChanged(s: Editable) {
-                binding.buttonSearch.isEnabled = s.length >= 3 && !viewModel.isSearchInProgress
-            }
-        })
-
-        binding.buttonSearch.setOnClickListener {
-            val request = binding.editText.text.toString()
-            viewModel.onButtonClick(request)
-        }
+        binding = FragmentMainBinding.inflate(layoutInflater)
         return binding.root
+    }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.viewModel = viewModel
+        binding.lifecycleOwner = viewLifecycleOwner
+        lifecycleScope.launch {
+            binding.invalidateAll()
+        }
+    }
 
+    override fun onSaveInstanceState(outState: Bundle) {
+        outState.putString("searchResult", viewModel.searchResult.value)
+        outState.putString("request", viewModel.request.value)
+        outState.putBoolean("isSearching", viewModel.isSearching.value)
+        super.onSaveInstanceState(outState)
+    }
+
+    override fun onViewStateRestored(savedInstanceState: Bundle?) {
+        super.onViewStateRestored(savedInstanceState)
+
+        // Восстанавливаем состояние ViewModel
+        viewModel._searchResult.value = savedInstanceState?.getString("searchResult") ?: ""
+        viewModel._request.value = savedInstanceState?.getString("request") ?: ""
+        viewModel._isSearching.value = savedInstanceState?.getBoolean("isSearching") ?: false
     }
 
 
--- app/src/main/java/ru/elenandreyuk/hw12/ui/main/MainViewModel.kt
+++ app/src/main/java/ru/elenandreyuk/hw12/ui/main/MainViewModel.kt
@@ -1,32 +1,57 @@
 package ru.elenandreyuk.hw12.ui.main
 
+import android.util.Log
+import android.view.View
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.Job
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
 
 class MainViewModel : ViewModel() {
-    private val _isSearching = MutableLiveData(false)
-    val isSearching: LiveData<Boolean> = _isSearching
+    internal val _isSearching = MutableStateFlow(false)
+    val isSearching: StateFlow<Boolean> = _isSearching
 
-    private val _searchResult = MutableLiveData<String>()
-    val searchResult: LiveData<String> = _searchResult
+    internal val _searchResult = MutableStateFlow("")
+    val searchResult: StateFlow<String> = _searchResult
 
-    var isSearchInProgress = false
-        private set
+    internal val _request = MutableStateFlow("")
+    val request: StateFlow<String> = _request
 
-    fun onButtonClick(request: String) {
-        isSearchInProgress = true
-        _isSearching.value = true
+    private var searchJob: Job? = null
 
-        viewModelScope.launch {
-            delay(5000)
-            isSearchInProgress = false
+    fun onTextChanged(view: View, string: String) {
+        if (string.length > 3) {
+            searchJob?.cancel()
+
+            searchJob = viewModelScope.launch {
+                _isSearching.value = true
+                _request.value = string
+                delay(300)
+                Log.d("edit", string)
+                delay(3000)
+
+                _isSearching.value = false
+                _searchResult.value = "По запросу $string ничего не найдено"
+            }
+        }else{
+            searchJob?.cancel()
+            _request.value = ""
+            Log.d("stop", string)
             _isSearching.value = false
-            _searchResult.value = "По запросу $request ничего не найдено"
+            _searchResult.value= ""
         }
     }
 
+    override fun onCleared() {
+        super.onCleared()
+        Log.d("clear", "clear")
+        searchJob?.cancel()
+    }
 }
+
+
